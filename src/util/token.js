dc0112625309df9d41b5ba6d4a654e092132233c
DanielScheffel <danioliveira.8292@gmail.com>
2023-05-15 11:55:01 -0300

    Conexao de dados

--- src/util/token.js
+++ src/util/token.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const checktoken = async (token, id, key) => jwt.verify(token, key, (err, decoded) => {
-
+    try{
+        if(decoded.id === id){
+            return true;
+        }
+        return false;
+    } catch (err) {
+        return false;
+    }
 });
 
 const setToken = async (id, key) => {
